7b81f4e363f038986fffa83c03017c8936ead30c
dd-lewis <ldenzel721@gmail.com>
2025-01-22 15:37:10 +0000

    update on user

--- SSL/urls.py
+++ SSL/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:choice>/", views.index, name="choice"),
-    path('', views.index, {'choice': 'home'}, name='home'),
-
+    path('home/', views.SSLView, {'choice': 'home'}, name='home'),
+    path('user/', views.SSLView, {'choice': 'user'}, name='user'),
+    path('complete/', views.SSLView, {'choice': 'complete'}, name='complete'),
     ]
